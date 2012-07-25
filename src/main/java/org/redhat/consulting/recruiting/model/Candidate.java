f72195c9db20181bf51f7816a33e8599d0a08635
Thomas Beauvais <tbeauvai@redhat.com>
2012-07-25 11:30:00 -0700

    Commit before moving..

---
+++ src/main/java/org/redhat/consulting/recruiting/model/Candidate.java
@@ -0,0 +1,11 @@
+package org.redhat.consulting.recruiting.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tbeauvais
+ * Date: 7/24/12
+ * Time: 10:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Candidate {
+}
